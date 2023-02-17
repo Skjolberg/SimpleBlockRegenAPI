13662b81d847398f48e9c26d393f773a2c311443
SKJOL <danielvazquezmunoz@gmail.com>
2023-02-17 18:12:14 +0100

    added material on top of the regeneration to the object

--- src/main/java/net/shibacraft/simpleblockregen/plugin/regeneration/Regeneration.java
+++ src/main/java/net/shibacraft/simpleblockregen/plugin/regeneration/Regeneration.java
@@ -24,11 +24,12 @@ public class Regeneration {
     private boolean removable;
     private Location firstLocation;
     private Material firstMaterial;
+    private Material topMaterial;
 
     public Regeneration(BlockList blockList, BlockState blockState, Material material, Location locCurrently,
                         Location locGenerate, long regenerationTime, long timeToRemove, BlockState replaceBlockState,
                         Material replaceMaterial, RegenerationMode mode, boolean removable, Location firstLocation,
-                        Material firstMaterial) {
+                        Material firstMaterial, Material topMaterial) {
         this.blockList = blockList;
         this.blockState = blockState;
         this.material = material;
@@ -42,14 +43,15 @@ public class Regeneration {
         this.removable = removable;
         this.firstLocation = firstLocation;
         this.firstMaterial = firstMaterial;
+        this.topMaterial = topMaterial;
     }
 
     public Regeneration(BlockList blockList, BlockState blockState, BlockState replaceBlockState, long regenerationTime,
                         Location locGenerate, Location locCurrently, String replaceBlock, long timeToRemove,
-                        RegenerationMode mode, boolean removable, Material type) {
+                        RegenerationMode mode, boolean removable, Material type, Material topMaterial) {
         this(blockList,blockState, blockState.getBlock().getType(), locCurrently, locGenerate,  regenerationTime,
                 timeToRemove, replaceBlockState,  Material.valueOf(replaceBlock), mode, removable,
-                blockState.getLocation(), type);
+                blockState.getLocation(), type, topMaterial);
     }
 
 }
